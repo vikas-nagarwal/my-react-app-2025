0ea5cca1ad0d2a8f6b3dd33ca9a45fb9c4facd39
vikas nagarwal <161242717+vikas-nagarwal@users.noreply.github.com>
2025-10-06 13:13:56 +0530

    class update
    class update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,9 +11,6 @@ class Car {
   }
 }
 
-
-
-
 // Rahul class
 class Rahul {
   constructor(name, age, id, roll) {
@@ -24,17 +21,33 @@ class Rahul {
   }
 }
 
+class vikas {
+  constructor(name, age, idnumber) {
+    this.name = name;
+    this.age = age;
+    this.idnumber = idnumber;
+  }
+}
+
+
+
 // Home component
 const Home = () => {
-  const myCar = new Car("BMW");       // Car object
+  const myCar = new Car("BMW"); // Car object
   const obj = new Rahul("Rahul", 28, 101, 12); // Rahul object
+  const ob4 = new vikas("vikas", 20, 25);
 
   return (
     <div>
       <div>
+        <h2>{ob4.name}</h2>
+        <h2>{ob4.age}</h2>
+        <h2>{ob4.idnumber}</h2>
+      </div>
+      <div>
         <h2>{myCar.present()}</h2>
       </div>
-  
+
       <div>
         <h1>{obj.name}</h1>
         <h2>{obj.age}</h2>
@@ -45,10 +58,4 @@ const Home = () => {
   );
 };
 
-
-
-
 export default Home;
-
-
-
