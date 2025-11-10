2ea52c3aa5f65cc950476e8144de71b2cb3a73d2
vikas nagarwal <161242717+vikas-nagarwal@users.noreply.github.com>
2025-11-10 18:47:15 +0530

    hh
    hgg

--- src/Pages/Practise.js
+++ src/Pages/Practise.js
@@ -1,31 +1,20 @@
-import { data } from "@remix-run/router";
-import { ResultType } from "@remix-run/router/dist/utils";
 import React from "react";
 
 function Practise() {
-  const ajaysharam = {
-    a: 50,
-    b: 55,
-    c: 40,
-    d: 49,
+  let a = 140;
+  let b = 20;
+  let c = a;
 
-    calc() {
-      if (this.a > this.b && this.a > this.c && this.a > this.d) {
-        return this.a; // a की value
-      } else if (this.b > this.a && this.b > this.c && this.b > this.d) {
-        return this.b; // b की value
-      } else if (this.c > this.a && this.c > this.b && this.c > this.d) {
-        return this.c; // c की value
-      } else {
-        return this.d; // d की value
-      }
-    },
-  };
+  a = b;
+  b = c;
 
-  console.log(ajaysharam.calc());
-
-  return <></>;
+  return (
+    <>
+      <h2>Day Message</h2>
+      <p>a: {a}</p>
+      <p>b: {b}</p>
+    </>
+  );
 }
-const result (ajaysharam.calc);
-export default Practise;
 
+export default Practise;
