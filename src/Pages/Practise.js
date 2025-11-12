3edb9f42966c72e66406a6bae9d1fd3746a00bfc
vikas nagarwal <161242717+vikas-nagarwal@users.noreply.github.com>
2025-11-12 18:31:47 +0530

    fgh
    ghgh

--- src/Pages/Practise.js
+++ src/Pages/Practise.js
@@ -1,20 +1,30 @@
 import React from "react";
 
 function Practise() {
-  let a = 140;
-  let b = 20;
-  let c = a;
+  const studentid = {
+    a: 20,
+    b: 30,
+    c: 40,
+  };
 
-  a = b;
-  b = c;
-
+  const message = studentid.a > 10 && studentid.b > studentid.a;
   return (
     <>
       <h2>Day Message</h2>
-      <p>a: {a}</p>
-      <p>b: {b}</p>
+      <h3 style={{ color: "blue" }}>{message}</h3>
+
+      <div
+        style={{
+          backgroundColor: "#f8f8f8",
+          padding: "10px",
+          borderRadius: "8px",
+        }}
+      >
+        <h2>Student A: {studentid.a}</h2>
+        <h2>Student B: {studentid.b}</h2>
+        <h2>Student C: {studentid.c}</h2>
+      </div>
     </>
   );
 }
-
 export default Practise;
